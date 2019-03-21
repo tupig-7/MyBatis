9d669e0794bd80c4bd13d402f8c2e4d79103516a
zdd520 <2664006323.qq.com>
2019-03-21 23:22:27 +0800

    First

---
+++ src/main/java/com/everythone/App.java
@@ -0,0 +1,13 @@
+package com.everythone;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
